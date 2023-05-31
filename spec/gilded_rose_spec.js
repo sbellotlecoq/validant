3d96c8623049cd051d49666674967c26251783da
sbellotlecoq <sbellotlecoq@gmail.com>
2023-05-31 20:47:21 +0200

    zou

---
+++ spec/gilded_rose_spec.js
@@ -0,0 +1,43 @@
+const { Shop, Item } = require("../src/gilded_rose.js");
+
+describe("Gilded Rose", function () {
+  it("full test", () => {
+    const items = [
+      new Item("+5 Dexterity Vest", 10, 20),
+      new Item("Aged Brie", 2, 0),
+      new Item("Elixir of the Mongoose", 5, 7),
+      new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+      new Item("Backstage passes to a TAFKAL80ETC concert", 5, 39),
+
+      // This Conjured item does not work properly yet
+      new Item("Conjured Mana Cake", 3, 6),
+    ];
+
+    const days = Number(process.argv[2]) || 2;
+    const gildedRose = new Shop(items);
+
+    for (let day = 0; day < days; day++) {
+      console.log(`\n-------- day ${day} --------`);
+      console.log("name, sellIn, quality");
+      items.forEach((item) =>
+        console.log(`${item.name}, ${item.sellIn}, ${item.quality}`)
+      );
+      gildedRose.updateQuality();
+      expect(items[0].name).toBe("+5 Dexterity Vest");
+      expect(items[0].sellIn).toBe(9);
+    	expect(items[0].quality).toBe(19);
+
+    }
+  });
+
+  it("should decrease sellIn and quality by 1 for normal items", function () {
+    const gildedRose = new Shop([new Item("foo", 2, 3)]);
+    const items = gildedRose.updateQuality();
+    expect(items[0].name).toBe("foo");
+    expect(items[0].sellIn).toBe(1);
+    expect(items[0].quality).toBe(2);
+  });
+});
